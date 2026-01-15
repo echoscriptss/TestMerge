5f933132605dbe31b382c0a86331363cbff1f6a8
Prabhjoot Singh <prabhjoot.singh@mobileprogramming.com>
2026-01-15 18:57:52 +0530

    added verify view

---
+++ TestMerge/Views/VerifyView.swift
@@ -0,0 +1,18 @@
+//
+//  VerifyView.swift
+//  TestMerge
+//
+//  Created by Mobile Programming on 15/01/26.
+//
+
+import SwiftUI
+
+struct VerifyView: View {
+    var body: some View {
+      Text("This is Verify View for testing")
+    }
+}
+
+#Preview {
+    VerifyView()
+}
